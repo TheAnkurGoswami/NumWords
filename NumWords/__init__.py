6f69ef2bfeab15772afdbd9f003d0e490a4e935d
TheAnkurGoswami <kinglusifer1401@gmail.com>
2024-04-27 19:01:47 +0530

    version update

--- NumWords/__init__.py
+++ NumWords/__init__.py
@@ -1,1 +1,2 @@
-from .core import numwords
+__version__ = "2.0.0"
+from .core import NumWords
--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 with open("./README.md", "r") as readme:
     description = readme.read()
 setup(
     name="numwords",
-    version="1.0.0",
+    version="2.0.0",
     description="",
     long_description=description,
     long_description_content_type="text/markdown",
@@ -13,11 +13,11 @@ setup(
     url="https://github.com/TheAnkurGoswami/NumWords",
     packages=["numwords"],
     install_requires=["typing"],
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     license="MIT License",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ]
+    ],
 )
